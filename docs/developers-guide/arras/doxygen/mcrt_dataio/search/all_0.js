5b5c773fa88873574b939d42163d2cecd936dc16
Toshi <toshikato@mac.com>
2023-02-08 10:07:25 -0800

    MOONRAY-4810 update doxygen data by latest main

--- docs/developers-guide/arras/doxygen/mcrt_dataio/search/all_0.js
+++ docs/developers-guide/arras/doxygen/mcrt_dataio/search/all_0.js
@@ -5,7 +5,6 @@ var searchData=
   ['clientreceiverfb',['ClientReceiverFb',['../classmcrt__dataio_1_1ClientReceiverFb.html',1,'mcrt_dataio']]],
   ['clientreceiverfb_2eh',['ClientReceiverFb.h',['../ClientReceiverFb_8h.html',1,'']]],
   ['clientreceiverstats',['ClientReceiverStats',['../classmcrt__dataio_1_1ClientReceiverStats.html',1,'mcrt_dataio']]],
-  ['clientreceiverstats_2eh',['ClientReceiverStats.h',['../ClientReceiverStats_8h.html',1,'']]],
   ['consoleautosetup',['consoleAutoSetup',['../classmcrt__dataio_1_1ClientReceiverFb.html#a2b5a339a388fe567dbdccee683dc2719',1,'mcrt_dataio::ClientReceiverFb']]],
   ['consoledriver',['consoleDriver',['../classmcrt__dataio_1_1ClientReceiverFb.html#afe3ca6801c699afe761a7cd3b4b56a5b',1,'mcrt_dataio::ClientReceiverFb']]],
   ['consoleenable',['consoleEnable',['../classmcrt__dataio_1_1ClientReceiverFb.html#a0c439d04b6be076a47f8b7a9a2bdfd69',1,'mcrt_dataio::ClientReceiverFb']]]
